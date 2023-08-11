cbe459305907e667f318faad10d64625ae447594
Gos <59265829+godstoge@users.noreply.github.com>
2023-08-11 12:10:57 +0200

    Update

--- patch_review.py
+++ patch_review.py
@@ -18,7 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+ 
 '''  # JSON Output
  "Vulnerability": [
     {
@@ -44,14 +44,111 @@
       ]
     },
     {
-      "Title": {
-        "Value": "Windows BitLocker Information Disclosure Vulnerability"
-      },
+      
+{'Title': {'Value': 'Microsoft Office Information Disclosure Vulnerability'}, 
+'Notes': [{'Title': 'Description', 'Type': 2, 'Ordinal': '0'}, {'Title': 'FAQ', 'Type': 4, 'Ordinal': '10', 'Value': '<p><strong>What type of information could be disclosed by this vulnerability?</strong></p>\n<p>The type of information that could be disclosed if an attacker successfully exploited this vulnerability is uninitialized memory.</p>\n'}, {'Title': 'FAQ', 'Type': 4, 'Ordinal': '10', 'Value': '<p><strong>Is the Preview Pane an attack vector for this vulnerability?</strong></p>\n<p>No, the Preview Pane is not an attack vector.</p>\n'}, {'Title': 'FAQ', 'Type': 4, 'Ordinal': '10', 'Value': '<p><strong>According to the CVSS metric, user interaction is required (UI:R). What interaction would the user have to do?</strong></p>\n<p>Exploitation of the vulnerability requires that a user open a specially crafted file.</p>\n<ul>\n<li>In an email attack scenario, an attacker could exploit the vulnerability by sending the specially crafted file to the user and convincing the user to open the file.</li>\n<li>In a web-based attack scenario, an attacker could host a website (or leverage a compromised website that accepts or hosts user-provided content) containing a specially crafted file designed to exploit the vulnerability.</li>\n</ul>\n<p>An attacker would have no way to force users to visit the website. Instead, an attacker would have to convince users to click a link, typically by way of an enticement in an email or instant message, and then convince them to open the specially crafted file.</p>\n'}, {'Title': 'Microsoft Office', 'Type': 7, 'Ordinal': '20', 'Value': 'Microsoft Office'}, {'Title': 'Microsoft', 'Type': 8, 'Ordinal': '30', 'Value': 'Microsoft'}], 
+'DiscoveryDateSpecified': 0, 
+'ReleaseDateSpecified': 0, 
+'CVE': 'CVE-2023-21714', 
+'ProductStatuses': [{'ProductID': ['11762', '11763', '11952', '11953'], 'Type': 3}], 
+'Threats': [{'Description': 
+				{'Value': 'Information Disclosure'}, 
+				'ProductID': ['11762'], 
+				'Type': 0, 
+				'DateSpecified': 0}, 
+			{'Description': {'Value': 'Information Disclosure'}, 'ProductID': ['11763'], 'Type': 0, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Information Disclosure'}, 'ProductID': ['11952'], 'Type': 0, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Information Disclosure'}, 'ProductID': ['11953'], 'Type': 0, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Important'}, 'ProductID': ['11762'], 'Type': 3, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Important'}, 'ProductID': ['11763'], 'Type': 3, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Important'}, 'ProductID': ['11952'], 'Type': 3, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Important'}, 'ProductID': ['11953'], 'Type': 3, 'DateSpecified': 0}, 
+			{'Description': {'Value': 'Publicly Disclosed:No;Exploited:No;Latest Software Release:Exploitation Less Likely;DOS:N/A'}, 'Type': 1, 'DateSpecified': 0}], 
+'CVSSScoreSets': [
+	{'BaseScore': 5.5, 'TemporalScore': 4.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:N/A:N/E:U/RL:O/RC:C', 'ProductID': ['11762']}, 
+	{'BaseScore': 5.5, 'TemporalScore': 4.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:N/A:N/E:U/RL:O/RC:C', 'ProductID': ['11763']}, 
+	{'BaseScore': 5.5, 'TemporalScore': 4.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:N/A:N/E:U/RL:O/RC:C', 'ProductID': ['11952']}, 
+	{'BaseScore': 5.5, 'TemporalScore': 4.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:N/A:N/E:U/RL:O/RC:C', 'ProductID': ['11953']}], 
+'Remediations': [{'Description': {'Value': 'Click to Run'}, 'URL': '', 'ProductID': ['11762', '11763', '11952', '11953'], 'Type': 2, 'DateSpecified': 0, 'AffectedFiles': [], 'RestartRequired': {'Value': 'No'}, 'SubType': 'Security Update', 'FixedBuild': 'https://aka.ms/OfficeSecurityReleases'}], 
+'Acknowledgments': [{'Name': [{'Value': 'willJ of vulnerability research institute'}], 'URL': ['']}], 
+'Ordinal': '37', 'RevisionHistory': [{'Number': '1.0', 'Date': '2023-02-14T08:00:00', 'Description': {'Value': '<p>Information published.</p>\n'}}]}, 
+      
       ...
 	  
+    For NIST så er Title bestandig tom.
+    {   
+        'Title': {},   
+        'Notes': [
+                {'Title': 'NIST NVD Details', 'Type': 6, 'Ordinal': '1', 'Value': 'https://nvd.nist.gov/vuln/detail/CVE-2023-0054'}, 
+                {'Title': 'Description', 'Type': 2, 'Ordinal': '0'}, 
+                {'Title': 'Mariner', 'Type': 7, 'Ordinal': '20', 'Value': 'Mariner'}, 
+                {'Title': 'security@huntr.dev', 'Type': 8, 'Ordinal': '30', 'Value': 'security@huntr.dev'}], 
+        'DiscoveryDateSpecified': False, 
+        'ReleaseDateSpecified': False, 
+        'CVE': 'CVE-2023-0054', 
+        'ProductStatuses': [{'ProductID': ['12139', '12140'], 'Type': 3}], 
+        'Threats': [{'Description': {}, 'ProductID': ['12139'], 'Type': 0, 'DateSpecified': False}, {'Description': {}, 'ProductID': ['12140'], 'Type': 0, 'DateSpecified': False}, {'Description': {}, 'ProductID': ['12139'], 'Type': 3, 'DateSpecified': False}, {'Description': {}, 'ProductID': ['12140'], 'Type': 3, 'DateSpecified': False}, {'Description': {'Value': 'DOS:N/A'}, 'Type': 1, 'DateSpecified': False}], 
+        'CVSSScoreSets': [{'BaseScore': 7.8, 'TemporalScore': 7.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H', 'ProductID': ['12139']}, {'BaseScore': 7.8, 'TemporalScore': 7.8, 'Vector': 'CVSS:3.1/AV:L/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H', 'ProductID': ['12140']}], 
+        'Remediations': [{'Description': {'Value': 'vim'}, 'URL': '', 'ProductID': ['12139', '12140'], 'Type': 2, 'DateSpecified': False, 'AffectedFiles': [], 'RestartRequired': {}, 'SubType': 'CBL-Mariner', 'FixedBuild': '9.0.1145-1'}], 
+        'Acknowledgments': [], 
+        'Ordinal': '8', 
+        'RevisionHistory': 
+
+    
+{'Title': {}, 
+'Notes': [
+	{'Title': 'NIST NVD Details', 'Type': 6, 'Ordinal': '1', 'Value': 'https://nvd.nist.gov/vuln/detail/CVE-2022-43552'}, 
+	{'Title': 'Description', 'Type': 2, 'Ordinal': '0'}, 
+	{'Title': 'Mariner', 'Type': 7, 'Ordinal': '20', 'Value': 'Mariner'}, 
+	{'Title': 'cve-assignments@hackerone.com', 'Type': 8, 'Ordinal': '30', 'Value': 'cve-assignments@hackerone.com'}], 
+'DiscoveryDateSpecified': 0, 
+'ReleaseDateSpecified': 0, 'CVE': 'CVE-2022-43552', 
+'ProductStatuses': [{'ProductID': ['12139', '12140'], 'Type': 3}], 
+'Threats': [
+	{'Description': {}, 'ProductID': ['12139'], 'Type': 0, 'DateSpecified': 0}, 
+	{'Description': {}, 'ProductID': ['12140'], 'Type': 0, 'DateSpecified': 0}, 
+	{'Description': {}, 'ProductID': ['12139'], 'Type': 3, 'DateSpecified': 0}, 
+	{'Description': {}, 'ProductID': ['12140'], 'Type': 3, 'DateSpecified': 0}, 
+	{'Description': {'Value': 'DOS:N/A'}, 'Type': 1, 'DateSpecified': 0}], 
+'CVSSScoreSets': [], 
+'Remediations': [
+	{'Description': {'Value': 'curl'}, 
+	'URL': '',
+	'ProductID': ['12139', '12140'], 
+	'Type': 2, 
+	'DateSpecified': 0, 
+	'AffectedFiles': [], 
+	'RestartRequired': {}, 
+	'SubType': 'CBL-Mariner', 
+	'FixedBuild': '7.86.0-3'}], 
+'Acknowledgments': [], 
+'Ordinal': '1', 
+'RevisionHistory': [{'Number': '1.0', 'Date': '2023-02-10T00:00:00', 'Description': {'Value': '<p>Information published.</p>\n'}}]}
+ 
+'''
+'''
+Gå gjennom hele release notes, 
+    Sjekk antall entries i en release note > Gjør avsjekk; stemmer denne med dict når ferdig.
+	Kartlegg CVEene i egen dict
+		hent ut CVE IDer
+		For CVE finn values in Threat (Remote Code Execution, EoP, osv) > Velg den som er mest bad
+		Har CVE noe greier ift. FAQ need to do things? - yay, nay
+		Er det en ADV -> Egen adv-dict
+
+{
+	'CVE': {'Value': 'CVExxxx'}
+	'Title': {'Value': 'Windows Graphics Component Remote Code Execution Vulnerability'},
+    'VulnerabilityType': {'Value': 'RCE'}
+    'ExploitLikelihood': {'Value': 'xxxxx'}
+	'CVSS': {'Value': '10.0'},
+	'EEPS': {'Value': '0'},
+    'FilteredFAQ': [{'FAQ1': 'xxxx'}
+    'Directlink': {'Value': 'xxxxx'}  #Kanskje ikke applicable for alle.
+	
+	
+		
 
 '''
-
 # TODO
 # Script sjekk om CVE-nummer starter med ADV (Januar har 93 vuln mot 118 linjer). Noen er oppdatert, så annen release date, men oppdatering januar. 
 # List out FAQs - Kanskje få ut alle FAQer - Det er jo som regel der det linkes til ADV og KB. Evt. gjøre sjekker på FAQer som ikke har boiler plate shit (Filtering by exemption) examples
@@ -83,7 +180,7 @@ import sys # for argv
 #import json
 
 # Global variables
-base_url = 'https://api.msrc.microsoft.com/cvrf/v2.0/'
+
 headers = {'Accept': 'application/json'}
 colwidth=0
 
@@ -92,30 +189,36 @@ vuln_types = [
     'Security Feature Bypass',
     'Remote Code Execution',
     'Information Disclosure',
-    'Denial of Service',
+    'Denial of Service',  
     'Spoofing',
-    'Edge - Chromium'
+    'Edge - Chromium',
+    'DOS'
     ]
 
 
 def count_type(search_type, all_vulns):
     counter = 0
     for vuln in all_vulns:
+        #print(type(vuln['Notes']))
+        for noteentries in vuln['Notes']: pass
+            #if noteentries.get('Title') == "NIST NVD Details": print("Basj")
         for threat in vuln['Threats']:
             if threat['Type'] == 0:
                 if search_type == "Edge - Chromium":
                     if threat['ProductID'][0] == '11655':
+                        if "NIST" in vuln['Notes']: print("AYE")
                         counter += 1
+                        #print(vuln["CVE"])
                         break
                 elif threat['Description'].get('Value') == search_type:
+                    #print(threat['Description'].get('Value'))
                     if threat['ProductID'][0] == '11655':
-                        # Do not double count Chromium Vulns
+                # Do not double count Chromium Vulns
                         break
-                    counter += 1
+                    counter += 1 # Count current search type
+                    if "NIST" in vuln['Notes']: print("AYE")
+                    #print(vuln["CVE"])
                     break
-        
-
-    
     return counter
 
 def count_exploited(all_vulns):
@@ -146,9 +249,9 @@ def exploitation_likely(all_vulns):
             cvss_score = cvss_sets[0].get('BaseScore', 0.0)
             
            
-        for threat in vuln['Threats']:
-            if threat['Type'] == 1:
-                description = threat['Description']['Value']
+        for threatentry in vuln['Threats']:
+            if threatentry['Type'] == 1:
+                description = threatentry['Description']['Value']
                 if 'Exploitation More Likely'.lower() in description.lower():
                     counter += 1
                     cves.append(f'{vuln["CVE"]} - {cvss_score} - {abbreviate_vuln(vuln["Title"]["Value"])}')
@@ -202,10 +305,15 @@ def abbreviate_vuln(arg):
     # down to something like "Windows Secure Socket Tunneling Protocol (SSTP) RCE"
     if cliargs.noabb: 
         return arg
+    if type(arg) == "NoneType":
+        return arg
     #Replace text and return arg
-    arg = arg.replace("Remote Code Execution Vulnerability","RCE")
-    arg = arg.replace("Elevation of Privilege Vulnerability","EoP")
-    arg = arg.replace("Security Feature Bypass Vulnerability","Security Feature Bypass")
+    try:
+        arg = arg.replace("Remote Code Execution Vulnerability","RCE")
+        arg = arg.replace("Elevation of Privilege Vulnerability","EoP")
+        arg = arg.replace("Security Feature Bypass Vulnerability","Security Feature Bypass")
+    except:
+        return arg
     return arg
 
 def insert_table_with_links(provided_cves):
@@ -214,8 +322,10 @@ def insert_table_with_links(provided_cves):
     # Go through entire json file to find the longest title.
     if colwidth == 0:
         for entry in all_vulns:     
-            if len(abbreviate_vuln(entry['Title']['Value'])) >= colwidth:
-                colwidth = len(abbreviate_vuln(entry['Title']['Value']))
+            try:
+                if len(abbreviate_vuln(entry['Title']['Value'])) >= colwidth:
+                    colwidth = len(abbreviate_vuln(entry['Title']['Value']))
+            except: pass
     else: 
         pass # Colwidth has already been determined.
 
@@ -226,7 +336,17 @@ def insert_table_with_links(provided_cves):
         cvewithlink.insert(1,"https://msrc.microsoft.com/update-guide/en-US/security-guidance/advisory/"+cve.split()[0]) 
         print(coloumns.format(*cvewithlink))
 
-
+def getReleaseNotes(date):
+    # Get the list of all vulns and return a dict
+    base_url = 'https://api.msrc.microsoft.com/cvrf/v2.0/'
+    get_sec_release = requests.get(f'{base_url}cvrf/{date}', headers=headers)
+    if get_sec_release.status_code != 200:
+        print(f"[!] Thats a {get_sec_release.status_code} from MS no release notes yet")
+        print(f"[!] {base_url}cvrf/{date}")
+        exit()
+    
+    releaseNotes_JSON = get_sec_release.json()
+    return releaseNotes_JSON
 
 if __name__ == "__main__":
     
@@ -287,22 +407,30 @@ if __name__ == "__main__":
         print("[+] Print current months Patch Tuesday stats.")
         # Use manual month list as datetimes month abbreviation is based on running systems locale
         list_months = ["Bogus-place-holder","jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
-        current_month = list_months[datetime.now().month] 
+        current_month = list_months[datetime.now().month]   # DEBUG  DEBUG  DEBUG  DEBUG  DEBUG 
+        #current_month = list_months[datetime.now().month] 
         date_to_fetch = str(datetime.now().year) + "-" + current_month
+
+
+    '''
+    all_vulns = releaseNotes_JSON.get('Vulnerability', [])
+    for 
+    numberOfVulnerabilities = len(all_vulns)
+    '''
         
-
-    # Get the list of all vulns
-    get_sec_release = requests.get(f'{base_url}cvrf/{date_to_fetch}', headers=headers)
-    if get_sec_release.status_code != 200:
-        print(f"[!] Thats a {get_sec_release.status_code} from MS no release notes yet")
-        print(f"[!] {base_url}cvrf/{date_to_fetch}")
-        exit()
-
-    release_json = get_sec_release.json()
-    #print(json.dumps(release_json))
-    title = release_json.get('DocumentTitle', 'Release not found').get('Value')
-    all_vulns = release_json.get('Vulnerability', [])
-    len_vuln = len(all_vulns)
+    '''
+    for rawvuln in all_vulns_raw:
+            if rawvuln['Title']:
+                #print(type(rawvuln))
+                #print(type(rawvuln['Notes']))                #{'Title': 'Description', 'Type': 2, 'Ordinal': '0', 'Value': '<p>This CVE was assigned by Chrome
+                all_vulns.append(rawvuln)
+                #print(rawvuln['Title'])'''
+    
+    releaseNotes = getReleaseNotes(date_to_fetch)
+    title = releaseNotes.get('DocumentTitle', 'Release not found').get('Value')
+    all_vulns = [] #dict
+    all_vulns = releaseNotes.get('Vulnerability', [])   # list
+    numberOfVulnerabilities = len(all_vulns)
 
 
 # Handle debug args
@@ -317,16 +445,16 @@ if __name__ == "__main__":
     print_header(title)
 
 # Printing a summary of all vulnerabilities
-    print(f'[+] Found a total of {len_vuln} vulnerabilities')
+    print(f'[+] Found a total of {numberOfVulnerabilities} vulnerabilities')
     itercount = 0
     for vuln_type in vuln_types:
 
         count = count_type(vuln_type, all_vulns)
         print(f'  [-] {count} {vuln_type} Vulnerabilities')
         itercount = itercount+count
-    if len_vuln == itercount: pass
+    if numberOfVulnerabilities == itercount: pass
     else: 
-        print(f'[!] ERROR - the summerization, {itercount} does not match the number of vulnerabilities {len_vuln} in the release notes. New vuln type?')
+        print(f'[!] ERROR - the summerization, {itercount} does not match the number of vulnerabilities {numberOfVulnerabilities} in the release notes. New vuln type?')
             
         # We are counting the number of entries in all_vulns match each entry of vuln_types. We add the different vulns together in itercount.
         # Now all_vulns does not match our itercount, which means there is a vulnerability in the release note which we have not 
